afb0118af7e0a3143dddd2766718923e70e2b13e
Dev_francis <okeke98@gmail.com>
2023-07-08 09:37:35 +0100

    nest: controlling the request and responses

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,11 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 import { CustomersService } from 'src/customers/services/customers/customers.service';
 
 @Controller('customers')
 export class CustomersController {
   constructor(private customersService: CustomersService) {}
-  @Get('')
-  getCustomer(): { id: number; name: string; email: string; createdAt: Date } {
-    return this.customersService.findCustomer();
+  @Get(':id')
+  getCustomer(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const customer = this.customersService.findCustomerById(id);
+    if (customer) {
+      res.send(customer);
+    } else {
+      res.status(404).send({ message: 'Customer not found' });
+    }
+  }
+
+  @Get('/search/:id')
+  searchCustomerById(@Param('id', ParseIntPipe) id: number) {
+    const customer = this.customersService.findCustomerById(id);
+    if (customer) return customer;
+    else throw new HttpException('Customer not found', HttpStatus.BAD_REQUEST);
   }
 }
